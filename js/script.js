bbc0fc81444257ad0cf3cfc10a38afb25a3f9e74
Youssef7594 <Yousseffaras75020@gmail.com>
2026-01-04 08:04:30 +0100

    Update: Formation and comment

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const navbar = document.querySelector('nav');
 // Fonction pour détecter le scroll
 window.addEventListener('scroll', function() {
     if (window.scrollY > 80) {
-        // Ajoute la classe 'scrolled' lorsque l'utilisateur a scrollé de plus de 50px
+        // Ajoute la classe 'scrolled' lorsque l'utilisateur a scrollé de plus de 80px
         navbar.classList.add('scrolled');
     } else {
         // Retire la classe 'scrolled' si l'utilisateur remonte en haut de la page
